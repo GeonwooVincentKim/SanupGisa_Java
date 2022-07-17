477a078bc78ea4a557c77ba87cf5790aa19f3197
GeonwooVincentKim <kdsnop@gmail.com>
2022-07-17 21:19:32 +0900

    [Feature] - Added abstract class

---
+++ src/Animal.java
@@ -0,0 +1,9 @@
+abstract class Animal {
+	String a = " is animal";
+
+	abstract void look();
+
+	void show() {
+		System.out.println("Zoo");
+	}
+}
---
+++ src/Chicken.java
@@ -0,0 +1,13 @@
+class Chicken extends Animal {
+	Chicken() {
+		look();
+	}
+
+	void look() {
+		System.out.println("Chicken " + a);
+	}
+
+	void display() {
+		System.out.println("two wings");
+	}
+}
